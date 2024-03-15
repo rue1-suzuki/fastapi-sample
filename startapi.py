a99dba6846de7c708dc3b6384d8f2a381aba537c
rue1-suzuki <suzuki.ryuichi.1998@gmail.com>
2024-03-15 18:22:54 +0900

    fix argv

--- startapi.py
+++ startapi.py
@@ -1,21 +1,26 @@
 import os
 
 
-def create_api():
-    api_name = os.sys.argv[1]
-
+def startapi(api_name: str):
     dir_name = f'./apis/{api_name}'
+
     if os.path.exists(dir_name):
-        print(f'{api_name}APIは既に存在します')
-        return
+        print(f'{api_name}API is already exists')
+        return False
 
     os.makedirs(dir_name)
     open(f'{dir_name}/__init__.py', 'w').close()
     open(f'{dir_name}/models.py', 'w').close()
     open(f'{dir_name}/routers.py', 'w').close()
     open(f'{dir_name}/views.py', 'w').close()
-    print(f'{api_name}APIを作成しました')
+    print(f'{api_name}API is created')
+    return True
 
 
 if __name__ == '__main__':
-    create_api()
+    if len(os.sys.argv) == 1:
+        print('API names is required')
+        os.sys.exit(1)
+
+    for api_name in os.sys.argv[1:]:
+        startapi(api_name)
